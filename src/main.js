851a433b200416982011d15c6625e09f88914ed7
Jcc <574805242@qq.com>
2016-08-04 19:29:12 +0800

    分离出router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 import Vue from 'vue'
 import App from './App'
 import VueRouter from "vue-router"
+import routerMap from './router'
 import filters from "./filters"
 
+
 Vue.use(VueRouter)
+
 
 Object.keys(filters).forEach((k) => {
   Vue.filter(k, filters[k])
@@ -12,64 +15,7 @@ Object.keys(filters).forEach((k) => {
 
 var router = new VueRouter()
 
-router.map({
-    '/': { //首页
-        name: 'list',
-        component: function(resolve) {
-            require(['./views/vList.vue'], resolve);
-        }
-    },
-    '*': {
-        component: function(resolve) {
-            require(['./views/vList.vue'], resolve);
-        }
-    },
-    '/list': { //首页
-        name: 'list',
-        component: function(resolve) {
-            require(['./views/vList.vue'], resolve);
-        }
-    },
-    '/topic/:id': {
-        name: "topic",
-        component: function(resolve) {
-            require(['./views/vTopic.vue'], resolve)
-        }
-    },
-    "/message": {
-        name: " message",
-        component: function(resolve) {
-            require(['./views/vMessage.vue'], resolve);
-        },
-        auth: true
-    },
-    "/about": {
-        name: " about",
-        component: function(resolve) {
-            require(['./views/vAbout.vue'], resolve);
-        }
-    },
-    "/perinfo/:loginname": {
-        name: " perinfo",
-        component: function(resolve) {
-            require(['./views/vPerinfo.vue'], resolve);
-        },
-        auth: true
-    },
-    "/login": {
-        name: " login",
-        component: function(resolve) {
-            require(['./views/vLogin.vue'], resolve);
-        }
-    },
-    "/newtopic": {
-        name: " newtopic",
-        component: function(resolve) {
-            require(['./views/vNewTopic.vue'], resolve);
-        },
-        auth: true
-    }
-})
+routerMap(router)
 
 
 router.beforeEach((transition) => {
